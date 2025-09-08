9ee70f5c24890b8d7019f0aee19782f2ddf68f9c
artem.pelevin <artem.pelevin@edgecenter.ru>
2025-09-08 15:17:33 +0300

    feat: add service layer

---
+++ service.py
@@ -0,0 +1,90 @@
+from typing import Any
+
+
+def create_tables(conn) -> None:
+    cur = conn.cursor()
+    cur.execute(
+        """
+CREATE TABLE IF NOT EXISTS news
+(
+    id             INTEGER PRIMARY KEY,
+    url            TEXT NOT NULL,
+    title          TEXT NOT NULL,
+    content        TEXT NOT NULL,
+    published_date TEXT NOT NULL,
+    created_at     TEXT DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TEXT DEFAULT CURRENT_TIMESTAMP
+);
+"""
+    )
+    cur.execute(
+        """
+CREATE TABLE IF NOT EXISTS prompts
+(
+    id         INTEGER PRIMARY KEY,
+    text       TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+"""
+    )
+    cur.execute(
+        """
+CREATE TABLE IF NOT EXISTS news_evaluations
+(
+    id          INTEGER PRIMARY KEY,
+    news_id     INTEGER NOT NULL,
+    model       TEXT    NOT NULL,
+    prompt_id   INTEGER NOT NULL,
+    scores      TEXT    NOT NULL,
+    final_score REAL    NOT NULL,
+    created_at  TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (news_id) REFERENCES news (id) ON DELETE CASCADE,
+    FOREIGN KEY (prompt_id) REFERENCES prompts (id) ON DELETE CASCADE
+);
+"""
+    )
+
+    cur.execute(
+        """
+CREATE TRIGGER IF NOT EXISTS update_news_timestamp
+AFTER UPDATE ON news
+BEGIN
+    UPDATE news SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+END;
+"""
+    )
+    cur.execute(
+        """
+CREATE TRIGGER IF NOT EXISTS update_prompts_timestamp
+AFTER UPDATE ON prompts
+BEGIN
+    UPDATE prompts SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+END;
+"""
+    )
+    cur.execute(
+        """
+CREATE TRIGGER IF NOT EXISTS update_news_evaluations_timestamp
+AFTER UPDATE ON news_evaluations
+BEGIN
+    UPDATE news_evaluations SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+END;
+            """
+    )
+    conn.commit()
+
+
+def add_prompt(conn, prompt: str) -> None:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO prompts (text) VALUES (?);", (prompt,))
+    conn.commit()
+
+
+def add_news_evaluation(conn, news_id: int, model: str, prompt_id: int, scores: dict[str, Any]) -> None:
+    final_score = sum(score for score in scores.values()) / len(scores.values())  # mean score
+    cur = conn.cursor()
+    cur.execute("INSERT INTO news_evaluations VALUES (?, ?, ?, ?, ?);", (news_id, model, prompt_id, scores, final_score))
+    conn.commit()
